0f02a13e021c955288cf60538f905887826c8ed1
Victor Hugo <victorhugo-correia@hotmail.com>
2022-06-21 20:20:55 -0300

    Exercicio 08 finalizado

---
+++ Fundamentos/Bloco_04/dia-01-JavaScript-primeiros-passos/Exercicio08.js
@@ -0,0 +1,13 @@
+const numero1 = 27;
+const numero2 = 37;
+const numero3 = 17;
+
+const umpar = numero1 % 2 === 0 || numero2 % 2 === 0 || numero3 % 2 === 0;
+
+if(umpar ===true){
+    console.log(true)
+}
+
+else {
+    console.log(false)
+}
--- Fundamentos/Bloco_04/dia-01-JavaScript-primeiros-passos/exercício5.js
+++ Fundamentos/Bloco_04/dia-01-JavaScript-primeiros-passos/exercício5.js
@@ -2,12 +2,12 @@ const angulo1 = 30;
 const angulo2 = 60;
 const angulo3 = 90;
 
-if (angulo1 + angulo2 + angulo3 == 180){
-    console.log(true)
+if (angulo1 < 0 && angulo2 < 0 && angulo3 <0) {
+    console.log("O valor inserido é invalido!")
 }
 
-else if (angulo1 < 0 && angulo2 < 0 && angulo3 <0) {
-    console.log("O valor inserido é invalido!")
+else if (angulo1 + angulo2 + angulo3 == 180){
+    console.log(true)
 }
 
 else {
